17350995efaa2f081d938d2d612b1101d5f365d6
ABHISHEK KR. GUPTA <abhishekkgupta20_ug@ece.nits.ac.in>
2024-08-22 13:00:55 +0530

    MOD:Reactive forms implemented in Login Component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, signal } from '@angular/core';
-import { FormBuilder,FormControl,FormGroup } from '@angular/forms';
+import { FormBuilder,FormControl,FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -13,6 +13,20 @@ export class LoginComponent {
   }
 
   loginForm!:FormGroup;
-  
+  email:string='';
+  password:string='';
+
+  constructor(private formbuilder:FormBuilder){
+    this.loginForm=formbuilder.group({
+      email:['',Validators.required],
+      password:['',Validators.required]
+    })
+  }
+
+  submitForm(){
+    console.log("login form is ON");
+    console.log(this.loginForm.controls);
+  }
+
   
 }
